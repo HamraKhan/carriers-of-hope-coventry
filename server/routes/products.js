b30f84aadf8e183902f336db1a908ec414a10ad5
mahri09 <mahriaa88@gmail.com>
2022-07-06 13:31:25 +0100

    added crud wit products table

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,4 +1,10 @@
-const express = require("express");
-const router = express.Router();
+let router = require("express").Router();
+let products = require('../dal/productsDal');
 
-module.exports = router;
+  router.post("/", products.createProduct); // Create a new Collection
+  router.get("/", products.getAllProducts);  // Retrieve all Collections
+  router.get("/:id", products.getOneProduct);  // Retrieve a single Collection with id
+  router.put("/:id", products.updateProduct);  // Update a Collection with id
+  router.delete("/:id", products.deleteProduct);  // Delete a Collection with id
+
+module.exports = router; 
