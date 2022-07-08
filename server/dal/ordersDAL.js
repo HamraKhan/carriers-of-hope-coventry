3df727212f25bbb76888771be87de97309b57dd1
Hamra <hamra.khan.b@gmail.com>
2022-07-08 18:52:55 +0100

    Implemented update logic on orders - added order_status column to orders table

--- server/dal/ordersDAL.js
+++ server/dal/ordersDAL.js
@@ -4,13 +4,45 @@ async function selectOrdersByMemberId(memberId) {
   return await pgClient.pool
     .query(
       `SELECT id, ` +
-        `order_date as orderDate, ` +
-        `order_ref as orderRef, ` +
-        `member_id as memberId ` +
-        `FROM orders where member_id = ${memberId};`);
+        `order_date AS orderDate, ` +
+        `order_ref AS orderRef, ` +
+        `member_id AS memberId ` +
+        `order_ref AS orderRef, ` +
+        `order_status AS orderStatus ` +
+        `FROM orders WHERE member_id = ${memberId};`);
 }
 
+async function insertOrder(orderObj) {
+  return await pgClient.pool.query(
+    "INSERT INTO orders (order_date, order_ref, member_id, order_status) " +
+      "VALUES ($1, $2, $3, $4) RETURNING *;",
+    [orderObj.orderDate, orderObj.orderRef, orderObj.memberId, orderObj.orderStatus]
+  );
+}
+
+async function insertOrderItems(orderItems) {
+  const orderItemsArray = orderItems.map(item => {
+    return [item.productId, item.orderId, item.quantity];
+  })
+  return await pgClient.pool.query(
+    format(
+      "INSERT INTO order_item (product_id, order_id, quantity) VALUES %L RETURNING *",
+      orderItemsArray
+    ),
+    []
+  );
+}
+
+function updateOrder(orderObj) {
+  return pgClient.pool.query(
+    `UPDATE orders SET order_status = $1 WHERE member_id = $2 AND order_ref = $3;`,
+    [orderObj.orderStatus, orderObj.memberId, orderObj.orderRef]
+  );
+}
 
 module.exports = {
-  selectOrdersByMemberId
+  selectOrdersByMemberId,
+  insertOrder,
+  insertOrderItems,
+  updateOrder,
 };
--- server/service/orders.js
+++ server/service/orders.js
@@ -3,29 +3,134 @@ const ordersDAL = require("../dal/ordersDAL");
 const memberService = require("./members");
 
 async function getOrders(userId) {
+  if (!memberService.isAdmin(userId)) {
+    const dbOrders = await ordersDAL.selectOrdersByMemberId(userId);
 
-    if (!memberService.isAdmin(userId)) {
+    return Promise.all(
+      dbOrders.rows.map(async (dbOrder) => {
+        const productList = await productService.getAllProductsByOrderId(
+          dbOrder.id
+        );
 
-      const dbOrders = await ordersDAL.selectOrdersByMemberId(userId)
-      
-      return Promise.all(
-        dbOrders.rows.map(async (dbOrder) => {
-          const productList = await productService.getAllProductsByOrderId(dbOrder.id);
+        return {
+          id: dbOrder.id,
+          orderdate: dbOrder.orderdate,
+          orderref: dbOrder.orderref,
+          products: productList,
+        };
+      })
+    ).then((ordersList) => {
+      return ordersList;
+    });
+  }
+}
 
-          return {
-            id: dbOrder.id,
-            orderdate: dbOrder.orderdate,
-            orderref: dbOrder.orderref,
-            products: productList,
-          };
-        })
-      ).then((ordersList) => {
-        return ordersList;
-      });
-    }
-
+async function addOrder(orderObj, userId) {
+  if (memberService.isAdmin(userId)) {
+    return {
+      statusCode: 401,
+      message: "unauthorised access - admin is not allowed to create orders.",
+    };
   }
 
+  const orderRef = Math.random().toString(36).substring(2, 9);
+  const currentDateTime = new Date();
+  const initialState = "pending";
+
+  const newOrderRecord = {
+    memberId: orderObj.memberId,
+    orderRef: orderRef,
+    orderDate: currentDateTime,
+    orderStatus: initialState,
+  };
+
+  const insertOrderResult = await ordersDAL.insertOrder(newOrderRecord);
+  if (!insertOrderResult.rowCount > 0) {
+    return {
+      statusCode: 500,
+      message: "order did not get inserted",
+    };
+  }
+
+  const orderId = insertOrderResult.rows[0].id;
+
+  const orderItems = orderObj.products.map((product) => {
+    return {
+      productId: product.id,
+      orderId: orderId,
+      quantity: product.quantity,
+    };
+  });
+
+  return ordersDAL
+    .insertOrderItems(orderItems)
+    .then((insertRes) => {
+      if (insertRes.rowCount === orderItems.length) {
+        return {
+          statusCode: 200,
+          message: { id: orderId },
+        };
+      } else {
+        return {
+          statusCode: 500,
+          message: "order did not get inserted",
+        };
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      return {
+        statusCode: 500,
+        message:
+          "an error occured on the server-side during the order insertion",
+      };
+    });
+}
+
+function orderUpdateAllowed(reqBody, userId) {
+  const isAdmin = memberService.isAdmin(userId);
+
+  if (isAdmin && ["APPROVED", "DECLINED"].includes(reqBody.orderStatus)) {
+    return true;
+  }
+  if (!isAdmin && "CANCELLED" === reqBody.orderStatus) {
+    return true;
+  }
+  return false;
+}
+
+async function modifyOrder(reqBody, userId) {
+  if (!orderUpdateAllowed(reqBody, userId)) {
+    return {
+      statusCode: 401,
+      message: "user is not allowed to update the condition",
+    };
+  }
+
+  const orderObj = {
+    id: reqBody.id,
+    memberId: reqBody.memberId,
+    orderRef: reqBody.orderRef,
+    orderStatus: reqBody.orderStatus,
+  };
+
+  const result = ordersDAL.updateOrder(orderObj);
+
+  if ((await result).rowCount === 1) {
+    return {
+      statusCode: 200,
+      message: "Your order status has been updated",
+    };
+  } else {
+    return {
+      statusCode: 204,
+      message: "Your order status has not been modified",
+    };
+  }
+}
+
 module.exports = {
-  getOrders
+  getOrders,
+  addOrder,
+  modifyOrder,
 };
